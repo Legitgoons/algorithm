78952b9924f197e6b38eefd54267efae6c45c137
Uichan Lee <101088491+Legitgoons@users.noreply.github.com>
2023-03-03 10:13:34 +0900

    [Silver III] Title: 돌 게임 3, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+if N%7 == 0 :
+    print("CY")
+elif N%7 == 1:
+    print("SK")
+elif N%7 == 2:
+    print("CY")
+elif N%7 == 3:
+    print("SK")
+elif N%7 == 4:
+    print("SK")
+elif N%7 == 5:
+    print("SK")
+elif N%7 == 6:
+    print("SK")
