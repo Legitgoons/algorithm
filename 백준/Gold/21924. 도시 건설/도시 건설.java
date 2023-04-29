e2173b57f1c0cec4b4f72d3906e9a2cee28c51c4
Uichan Lee <101088491+Legitgoons@users.noreply.github.com>
2023-04-30 00:52:23 +0900

    [Gold IV] Title: 도시 건설, Time: 1980 ms, Memory: 187492 KB -BaekjoonHub

---
+++ 백준/Gold/21924. 도시 건설/도시 건설.java
@@ -0,0 +1,76 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] p; // 대표를 저장할 배열
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken()); // V : 정점의 개수, 0부터 시작
+        int E = Integer.parseInt(st.nextToken()); // E : 간선의 수
+        long sum = 0;
+
+        int[][] edges = new int[E][3]; // 간선 정보 입력받아서 배열에 저장
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            edges[i][0] = Integer.parseInt(st.nextToken());// 시작 정점
+            edges[i][1] = Integer.parseInt(st.nextToken());// 끝 정점
+            edges[i][2] = Integer.parseInt(st.nextToken());// 가중치
+            sum += edges[i][2];
+        }
+
+        // 크루스칼 1단계 : 간선 오름차순 정렬
+        Arrays.sort(edges, new Comparator<int[]>() {
+            public int compare(int[] o1, int[] o2) {
+                return o1[2] - o2[2];
+            }
+        });
+
+        // 크루스칼 2단계 : 사이클이 발생하지 않는 V-1개의 간선 선택
+        // make-set(자신을 대표로 초기화)
+        p = new int[V+1];
+        for (int i = 0; i < V+1; i++) {
+            p[i] = i;
+        }
+
+        long ans = 0; // 최소비용
+        int pick = 0; // 선택한 간선의 수
+
+        for (int i = 0; i < E; i++) {
+            int px = findset(edges[i][0]);
+            int py = findset(edges[i][1]);
+
+            if (px != py) { // 부모가 다르다면 = 사이클이 발생하지 않는다면
+                union(px, py); // union으로 합쳐줌
+                ans += edges[i][2];
+                pick++;
+            }
+            if (pick == V-1)
+                break;
+        }
+        if(pick != V-1)
+            System.out.println(-1);
+        else
+            System.out.println(sum - ans);
+    }
+
+    // path compression (Findset 과정에서 만나는 모든 노드들이 직접 root를 가리키도록 조정)
+    static int findset(int x) {
+        if (x != p[x]) // x가 자신의 부모값이 아니라면
+            p[x] = findset(p[x]); // 재귀로 부모값을 찾도록 함
+        return p[x]; // 부모값을 찾았다면 return
+    }
+
+    static void union(int x, int y) { // 부모값이 작은쪽이 부모값이 큰쪽에 붙도록
+        if (p[y] < p[x])
+            p[y] = findset(x);
+        else
+            p[x] = findset(y);
+    }
+}
